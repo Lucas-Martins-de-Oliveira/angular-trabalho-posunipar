4695b065e39f5ab36c92e8626cf03dffc9610fe4
Jean Junior Heldt <jean.heldt@gmail.com>
2021-06-17 09:24:17 -0300

    Ajustado imports do app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpConfigInterceptor } from './shared/guards/http.interceptor';
 import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
+    CommonModule,
+    ToastrModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
